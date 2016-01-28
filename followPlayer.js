a9b1a9ab35e234c99079b3afedf527fa0ce7f5e6
Matthew Walcher <mwalcher@live.ca>
2016-01-27 22:36:37 -0500

    added camera script and script for player controller

---
+++ followPlayer.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+private var target : GameObject;
+
+function Start(){
+	target = GameObject.Find("Succubus");
+}
+
+function Update(){
+	transform.position.x = target.transform.position.x+3.5;
+	transform.position.y = target.transform.position.y+.5;
+}
---
+++ playerController.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var speed : float;
+public var flyHeight : float;
+public var gravity : float;
+
+// Disable Gravity
+GetComponent.<Rigidbody>().useGravity = false;
+
+function FixedUpdate(){
+	// Locked Z Position
+	transform.position.z = 0;
+
+	// Constant Horzontal Movement
+	GetComponent.<Rigidbody>().velocity.x = speed;
+	
+	// Apply New Gravity
+	GetComponent.<Rigidbody>().AddForce(new Vector3(0,-gravity*GetComponent.<Rigidbody>().mass,0));
+	
+	// Handle Flying
+	if(Input.GetButton("Jump")){
+		GetComponent.<Rigidbody>().velocity.y = flyHeight;
+	}
+
+}
