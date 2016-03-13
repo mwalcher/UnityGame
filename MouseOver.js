e1fd919238c6aae228a1b604640a77334c8eb55c
Rusu <v_rusu@tss-hr312-dm35.local>
2016-03-13 12:28:31 -0400

    Hover for Character Selection

---
+++ MouseOver.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+
+public var GUI :  GameObject;
+private var ScreenSelectionGUI :  ScreenSelectionGUI;
+public var lightObj : Light;
+public var myText : Text;
+private var startingColor;
+
+
+function Start () {
+	startingColor = myText.color;
+	ScreenSelectionGUI = GUI.GetComponent.<ScreenSelectionGUI>();
+}
+
+function Update () {
+
+}
+
+function OnMouseEnter() {
+	Debug.Log('mouseover');
+	myText.color = new Color(255,255,255,1);
+
+	lightObj.enabled = true;
+
+}
+
+function OnMouseExit() {
+	Debug.Log('mouseout');
+
+	lightObj.enabled = false;
+	myText.color = startingColor;
+}
