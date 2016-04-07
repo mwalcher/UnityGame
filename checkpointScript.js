dd4ad947385420342ed3e9aa29d4f5fdacf4aaea
Matthew Walcher <mwalcher@live.ca>
2016-04-07 10:16:21 -0400

    checkpoint message script

--- checkpointScript.js
+++ checkpointScript.js
@@ -3,9 +3,11 @@
 private var player : GameObject;
 private var target : Vector3;
 private var flag : Vector3;
+public var checkpointMessage : GameObject;
 
 function Start(){
 	player = GameObject.Find("Player");
+	checkpointMessage.SetActive(false);
 }
 
 function Update(){
@@ -14,8 +16,19 @@ function Update(){
 
     if(gameObject.name == "Checkpoint1" && target.x >= flag.x && !GameState.getCheck1()){
     	GameState.setCheck1(target,GameState.getTotalGems());
+    	displayMessage();
     }else if(gameObject.name == "Checkpoint2" && target.x >= flag.x && !GameState.getCheck2()){
     	GameState.setCheck2(target,GameState.getTotalGems());
+    	displayMessage();
     }
 
+}
+
+function displayMessage(){
+	checkpointMessage.SetActive(true);
+	Invoke("hideMessage", 2);
+}
+
+function hideMessage(){
+	checkpointMessage.SetActive(false);
 }
